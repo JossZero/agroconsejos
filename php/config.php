a381d48d182a121a50f14d42156ce28a87994b80
José Emiliano Hernández Macias <117881241+JossZero@users.noreply.github.com>
2025-10-19 21:19:04 -0600

    config.php

--- php/config.php
+++ php/config.php
@@ -3,7 +3,7 @@ class Config {
     // Base de datos
     const DB_HOST = 'localhost';
     const DB_USER = 'agroapp';
-    const DB_PASS = 'MiNuevaClaveSegura123';
+    const DB_PASS = '12345';
     const DB_NAME = 'agroconsejos';
     const DB_PORT = 3306;
 
